b025b4bbbec4637266e281fc13f06b2f0ec43318
John Sasser <cw.john.sasser@gmail.com>
2020-02-07 18:30:37 -0500

    bulk of the app. still needs styling on the lower cards in the html

--- generatedHtml.js
+++ generatedHtml.js
@@ -3,7 +3,7 @@ const colors = {
 		wrapperBackground: "#E6E1C3",
 		headerBackground: "#C1C72C",
 		headerColor: "black",
-		photoBorderColor: "#black"
+		photoBorderColor: "black"
 	},
 	blue: {
 		wrapperBackground: "#5F64D3",
@@ -25,8 +25,7 @@ const colors = {
 	}
 };
 
-function generateHTML(data) {
-	console.log(data);
+function generatedHTML(data) {
 	return `<!DOCTYPE html>
 <html lang="en">
    <head>
@@ -45,34 +44,36 @@ function generateHTML(data) {
          *::before {
          box-sizing: border-box;
          }
+
          html, body {
          padding: 0;
          margin: 0;
          }
+
          html, body, .wrapper {
          height: 100%;
          }
+
          .wrapper {
          background-color: ${colors[data.color].wrapperBackground};
          padding-top: 100px;
          }
+
          body {
          background-color: white;
          -webkit-print-color-adjust: exact !important;
          font-family: 'Cabin', sans-serif;
          }
+
          main {
          background-color: #E9EDEE;
          height: auto;
          padding-top: 30px;
          }
-         h1, h2, h3, h4, h5, h6 {
-         font-family: 'BioRhyme', serif;
-         margin: 0;
-         }
+
          h1 {
          font-size: 3em;
-         }
+         } 
          h2 {
          font-size: 2.5em;
          }
@@ -88,20 +89,27 @@ function generateHTML(data) {
          h6 {
          font-size: 1.2em;
          }
+         h1, h2, h3, h4, h5, h6 {
+         font-family: 'BioRhyme', serif;
+         margin: 0;
+         }
+
+         
          .photo-header {
          position: relative;
          margin: 0 auto;
          margin-bottom: -50px;
-         display: flex;
-         justify-content: center;
-         flex-wrap: wrap;
+         display: block;
+         text-align: center;
          background-color: ${colors[data.color].headerBackground};
          color: ${colors[data.color].headerColor};
          padding: 10px;
          width: 95%;
          border-radius: 6px;
          }
+
          .photo-header img {
+         margin: 0 auto;
          width: 250px;
          height: 250px;
          border-radius: 50%;
@@ -110,29 +118,35 @@ function generateHTML(data) {
          border: 6px solid ${colors[data.color].photoBorderColor};
          box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
          }
+
          .photo-header h1, .photo-header h2 {
          width: 100%;
          text-align: center;
          }
+
          .photo-header h1 {
          margin-top: 10px;
          }
+
          .links-nav {
          width: 100%;
          text-align: center;
          padding: 20px 0;
          font-size: 1.1em;
          }
+
          .nav-link {
          display: inline-block;
          margin: 5px 10px;
          }
+
          .workExp-date {
          font-style: italic;
          font-size: .7em;
          text-align: right;
          margin-top: 10px;
          }
+
          .container {
          padding: 50px;
          padding-left: 100px;
@@ -156,8 +170,12 @@ function generateHTML(data) {
          }
          
          .col {
-         flex: 1;
+         flex: 1
          text-align: center;
+         }
+
+         .left {
+           float: left;
          }
 
          a, a:hover {
@@ -171,11 +189,80 @@ function generateHTML(data) {
             zoom: .75; 
           } 
          }
+
       </style>
       <body> 
-         <img src='${data.profileImg}'>
+        <div class='wrapper'>
+
+          <div class="photo-header img">
+            <img class="img" src='${data.profileImg}' alt='Profile Photo'>
+
+              <h1>Howdy,</h1>
+                 <br>
+              <h2>My name is ${data.username}!</h2>
+              <h4 class="col">Am I currently hireable, you ask? ${
+								data.hireable
+							};</h4>
+         
+              <br>
+              <br>
+
+            <div class="nav links-nav">
+              <a class="nav-link btn-social-icon btn-twitter" href="https://www.google.com/maps/place/${
+								data.location
+							}" ${data.location}><i class="fas fa-map-marker-alt"></i></a>
+
+              <a class="nav-link btn-social-icon btn-twitter" href="${
+								data.profile
+							}"><i class="fab fa-github fa-lg"></i></a>
+
+              <a class="nav-link" href="${
+								data.blog
+							}"><i class="fas fa-blog"></i></a>
+        
+            </div>
+          </div>
+
+          <div class="container">
+        <div class="row">
+          <h5 class="col">
+            ${data.userBio}
+          </h5>
+        </div>
+
+        <div class="row left">
+
+          <div class="card col">
+            <h2>
+              Public Repos: ${data.repos}
+            </h2>
+          </div>
+          <div class="card col">
+            <h2>
+              GitHub Stars: ${data.stars}
+            </h2>
+          </div>
+        </div>
+        <div class="row left">
+          <div class="card col">
+            <h2>
+              Following: ${data.following}
+            </h2>
+          </div>
+          <div class="card col">
+            <h2>
+              Followers: ${data.followers}
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      </div>
+      <script src="https://kit.fontawesome.com/5f4b0fb1a5.js" crossorigin="anonymous"></script>
       </body>`;
 }
 
 // html-pdf from npm to convert;
-module.exports = generateHTML;
+module.exports = {
+	generatedHTML: generatedHTML
+};
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const axios = require("axios");
-// const input = process.argv[2];
-// const color = process.argv[3];
+const pdf = require("html-pdf");
 const inquirer = require("inquirer");
 const generatedHtml = require("./generatedHtml");
+
+var htmlSTR = fs.readFileSync("./index.html", "utf8");
+console.log(typeof htmlSTR);
 
 const questions = [
 	{
@@ -24,61 +26,91 @@ function init() {
 	// & axios `res.data` so we can push a single object to generatedHtml to return the completed html doc;
 	const data = {};
 
-	inquirer
-		.prompt(questions)
-		.then(answers => {
-			let gitUsers = `https://api.github.com/users/${answers.username}`;
-			let gitStars = `https://api.github.com/users/${answers.username}/starred`;
+	inquirer.prompt(questions).then(answers => {
+		let gitUsers = `https://api.github.com/users/${answers.username}`;
+		let gitStars = `https://api.github.com/users/${answers.username}/starred`;
 
-			const requestGitUsers = axios.get(gitUsers);
-			const requestGitStars = axios.get(gitStars);
+		const requestGitUsers = axios.get(gitUsers);
+		const requestGitStars = axios.get(gitStars);
 
-			data.color = answers.color.toLowerCase();
-			data.username = answers.username;
+		data.color = answers.color.toLowerCase();
+		data.username = answers.username;
 
-			return axios.all([requestGitUsers, requestGitStars]).then(
-				axios.spread((...res) => {
-					const responseGitUsers = res[0];
-					const responseGitStars = res[1];
+		return axios
+			.all([requestGitUsers])
+			.then(() => {
+				return axios.all([requestGitUsers, requestGitStars]).then(
+					axios.spread((...res) => {
+						const responseGitUsers = res[0];
+						const responseGitStars = res[1];
 
-					// res from res[0]
-					data.profileImg = responseGitUsers.data.avatar_url;
-					data.location = responseGitUsers.data.location;
-					data.profile = responseGitUsers.data.html_url;
-					data.blog = responseGitUsers.data.blog;
-					data.username = responseGitUsers.data.name;
-					data.userBio = responseGitUsers.data.bio;
-					data.repos = responseGitUsers.data.public_repos;
-					data.followers = responseGitUsers.data.followers;
-					data.following = responseGitUsers.data.following;
-					// res from res[1]
-					console.log(responseGitStars.data.length);
-					data.stars = responseGitStars.data.length;
+						// res from res[0]
+						data.profileImg = responseGitUsers.data.avatar_url;
+						data.hireable = responseGitUsers.data.hireable;
+						// if (data.hireable == true) {
+						// 	return "yes";
+						// } else return "no";
 
-					// calling the html function from generatedHtml.js,
-					// putting the data obj into the html,
-					// needs the template literal keys in html to know where to go;
-					const html = generatedHtml(data);
-					console.log(html);
-				})
-			);
-		})
-		.catch(error => {
-			throw error;
-		});
-}
+						data.location = responseGitUsers.data.location;
+						data.profile = responseGitUsers.data.html_url;
+						data.blog = responseGitUsers.data.blog;
 
-init();
+						data.username = responseGitUsers.data.name;
+						data.userBio = responseGitUsers.data.bio;
+						console.log(responseGitUsers.data.bio);
 
-function writeToFile(fileName, data) {
-	// the return is an object and we need to JSON.stringify it to work with it.
-	fs.writeFile("index.html", data, "utf8", (err, data) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("Write file completed!");
-		}
+						data.repos = responseGitUsers.data.public_repos;
+						data.followers = responseGitUsers.data.followers;
+						data.following = responseGitUsers.data.following;
+
+						// res from res[1]
+						data.stars = responseGitStars.data.length;
+
+						// responseGitStars.data.stargazers_count.forEach();
+
+						console.log(data);
+
+						// calling the html function from generatedHtml.js,
+						// putting the data obj into the html,
+						// needs the template literal keys in html to know where to go;
+
+						const html = generatedHtml.generatedHTML(data);
+
+						// fs.writeFile("index.html", html, "utf8", err => {
+						// 	if (err) {
+						// 		console.log(err);
+						// 	} else {
+						// 		console.log("Write file completed!");
+						// 	}
+						// });
+
+						const pdfOptions = { format: "Letter", orientation: "portrait" };
+						pdf
+							.create(html, pdfOptions)
+							.toFile("./profile.pdf", function(err, res) {
+								if (err) return console.log(err);
+								console.log(res); // { filename: '/app/businesscard.pdf' }
+							});
+					})
+				);
+			})
+			.catch(error => {
+				throw error;
+			});
 	});
 }
-
 init();
+
+// not working, don't know why.  No-one knows why;
+
+// function writeToFile(fileName, data) {
+// 	// the return is an object and we need to JSON.stringify it to work with it.
+// 	// const fileName = "generatedIndex.html";
+// 	fs.writeFile("index.html", html, "utf8", (err, data) => {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log("Write file completed!");
+// 		}
+// 	});
+// }
